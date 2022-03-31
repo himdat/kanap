b302eca548e003f553f2eae5c3544ff7407b0d1b
himdat <himdatahamed1@gmail.com>
2022-03-31 18:03:52 +0200

    fff

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,24 @@
 // récup produittab qui est dans le locals dans le panier
-let addProduit = JSON.parse(localStorage.getItem("produit"));// pour le retranscrire en js
-console.log(addProduit);
+let cartProduct = JSON.parse(localStorage.getItem("produit"));// pour le retranscrire en js
+
+
+//TODO 1 . Récupération des items dans le local storage
+
+//TODO 2. Iteration des items dans le local storage pour faire appel api sur produit detail pour enrichir le DOM
+const listIds = [];
+cartProduct.forEach(item => {
+    listIds.push(item.id)
+})
+console.log(listIds);
+//TODO 3. - EERFD-ATERF
+const objectToSendOrder = {
+    products: listIds,
+    contact:{
+        firstname
+    }
+}
+
+
+
 
    
--- front/js/product.js
+++ front/js/product.js
@@ -7,27 +7,18 @@ let prix = document.getElementById("price");
 let titre = document.getElementById("title");
 let description = document.getElementById("description");
 let couleur = document.getElementById("colors");
-let quantitySelector = parseInt(document.getElementById("quantity"));
-
-
-//stocker les éléments du produit dans un tableau
-let produitData = [];
 
 //récup des données de l'api ou est stocké les canapés
 // asynch et await tu attend la résolution de la promise
 const fetchProduit = async () => {
-    await fetch(`http://localhost:3000/api/products/${produit}`)
-    .then((res) => res.json())
-    .then((promise) => {
-        produitData = promise;
-        console.log(produitData);
-    });
-    
+    const response = await fetch(`http://localhost:3000/api/products/${produit}`);
+
+    return await response.json();
 };
 
 // pour afficher les infos de chaque produit de l'accueil
 const produitDisplay = async () => {
-    await fetchProduit();
+    const produitData = await fetchProduit();
 
     titre.innerHTML = produitData.name;
     image.innerHTML = `<img src="${produitData.imageUrl}" alt="${produitData.altTxt}"></img>`;
@@ -39,48 +30,59 @@ const produitDisplay = async () => {
     couleur.innerHTML += `
     <option value="${produitData.colors[i]}">${produitData.colors[i]}</option>`;
   }
-  addBasket(produitData);
- 
+};
+
+const addBasketListener = () => {
+    const bouton = document.getElementById("addToCart");
+    bouton.addEventListener("click", () => {
+        let produitTableau = JSON.parse(localStorage.getItem("produit")); // parse(tranformer en objet js)
+        const select = document.getElementById("colors");
+        // fpc est nouveau objet avec color et quantite
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const id = urlSearchParams.get('id');
+        const quantity = parseInt(document.getElementById('quantity').value)
+        const color = select.value;
+        if (quantity < 1) {
+            alert('Vous devez choisir au moins une quantité égale à 1 produit.')
+            return;
+        }
+        const objectToAdd = {
+            id: id,
+            color: color,
+            quantite: quantity
+        }
+        console.log(objectToAdd);
+        if(produitTableau == null) { // si ptab est null, ptab sera un tableau vide
+            console.log(produitTableau);
+            produitTableau = [];
+            console.log(produitTableau);
+        }
+            //TODO 1 . Chercher la présence d'un produit existant dans le panier
+            // [
+            //     {
+            //         id: '223',
+            //         color: 'Blue',
+            //         quantite
+            //     },
+            //     {
+            //         id: '223',
+            //         color: 'Blue',
+            //         quantite
+            //     }
+            // ]
+            const cartFiltered = produitTableau.filter(item => item.id === id && item.color === color);
+            console.log(cartFiltered);
+            if (cartFiltered.length === 0) {
+                //TODO 1.1 Si pas de produit -> j'ajoute le produit au tableau
+                produitTableau.push(objectToAdd);
+            } else {
+                //TDOO 1.2 Si produit -> J'additionne quantité existante avec nouvelle quantité reçu
+                cartFiltered[0].quantite += quantity
+            }
+            // on va transformer ptab en string pour le stocker dans le localstorage, puis on lui dire tu va prendre une clé qui s'appelle produit et valeur qui sera à l'intérieur sera une string du tableau et voila comment on stocke dans le localS
+            localStorage.setItem("produit", JSON.stringify(produitTableau));
+    });
 };
 
 produitDisplay();
-
-
-const addBasket = () => {
-  let bouton = document.getElementById("addToCart");
-  console.log(bouton);
-  bouton.addEventListener("click", () => {
-    let produitTableau = JSON.parse(localStorage.getItem("produit")); // parse(tranformer en objet js)
-    let select = document.getElementById("colors");
-    console.log(select.value);
-    console.log(produitTableau);
-    // fpc est nouveau objet avec color et quantite
-     const fusionProduitColor = Object.assign({}, produitData, {// qu permet d'assigner et rajouter qlqc à unob
-       color: `${select.value}`,
-       quantite: 1,
-     }); 
-     console.log(fusionProduitColor);
-    // on veut que si il y a rien, on rajoute des produits
-
-    if(produitTableau == null){ // si ptab est null, ptab sera un tableau vide
-      produitTableau = [];
-      // on va pousser fusionproduitcolor, si tu a rien dans localstorage(ptab), tu m'execute ce code
-      produitTableau.push(fusionProduitColor);
-      console.log(produitTableau);
-      // on va transformer ptab en string pour le stocker dans le localstorage, puis on lui dire tu va prendre une clé qui s'appelle produit et valeur qui sera à l'intérieur sera une string du tableau et voila comment on stocke dans le localS
-      
-    } else{
-      const existProduct = produitTableau.filter(element => {
-        return element.id === produit && element.color === couleur;
-      })
-      if(existProduct.length > 0){
-        existProduct[0].quantity += quantitySelector;
-      }else{
-        let fusionProduitColor;
-        produitTableau.push(fusionProduitColor);
-      }
-
-    }
-    localStorage.setItem("produit", JSON.stringify(produitTableau));
-  });
-};
+addBasketListener();
